9ea59f5ec00ca5460c037fe777773ff0f0e7780b
PitbulG <90787634+PitbulG@users.noreply.github.com>
2021-09-15 20:43:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import smtplib as root
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def send_mail():
+    login = raw_input('Vvedite vashy pochty')
+    password = raw_input('vvedite porol')
+    url = raw_input('URL:')
+    toaddr = raw_input('komy')
+    topic = raw_input('tema')
+    massege = raw_input('vvedite soobschenie:')
+
+    msg = MIMEMultipart()
+
+    msg['Subject'] = topic
+    msg['From'] = login
+    body = massage
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = root.SMTP_SSL(url, 465)
+    server.login(login, password)
+    server.sendmail(login, toaddr, msg.as_string())
+
+
+def main():
+    send_main()
+
+
+if __name__ == '__main__':
+    main()
+
